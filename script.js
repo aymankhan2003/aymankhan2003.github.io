a0bf9ddeaddf7276803a14509a393d93e679d524
Ayman Awsaf Khan <aymankhaninfo@gmail.com>
2020-08-04 15:01:45 +0000

    I added some prompt buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var name = prompt("Input Your Name")
+var year = prompt("Input Your Age")
+var welcome = document.getElementById("welcome")
+welcome.innerHTML = "Welcome " + name +"." + " Hope you Enjoy!";
+var age = document.getElementById("age")
+age.innerHTML = "I hear you are " + year + " years old.";
+age.style.color = "Blue";
+welcome.style.color = "Blue";
+
