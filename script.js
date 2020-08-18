d9c304993a0892f726c18dd88ce1f3472f32190a
Ayman Awsaf Khan <aymankhaninfo@gmail.com>
2020-08-18 15:19:03 +0000

    addeda page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function redirect(){
-  windows.location.href = "aboutyou.html"
+
 var name = prompt("Input Your Name")
 var year = prompt("Input Your Age")
 var welcome = document.getElementById("welcome")
@@ -8,4 +7,4 @@ var age = document.getElementById("age")
 age.innerHTML = "I hear you are " + year + " years old.";
 age.style.color = "Blue";
 welcome.style.color = "Blue";
-}
+
